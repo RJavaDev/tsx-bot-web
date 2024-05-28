a656c1cd2bf9251dda950d165952bb66de7be056
Пользователь <xorozovshohzod@gmail.com>
2024-05-28 10:26:30 +0500

    change searchFilter.js file and remove chunk files

--- src/componenta/SearchFilter.js
+++ src/componenta/SearchFilter.js
@@ -1,59 +1,65 @@
 // SearchFilter.js
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Input, Button, Form } from 'antd';
-import { setSearchParams, setIsSearching } from './utils/searchSlice';
-import { fetchData } from './request/Api';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Input, Button, Form } from "antd";
+import { setSearchParams, setIsSearching } from "./utils/searchSlice";
+import { fetchData } from "./request/Api";
 import BASE_URL from "./utils/config";
 
 const SearchFilter = () => {
-    const dispatch = useDispatch();
-    const searchParams = useSelector((state) => state.search.searchParams);
-    const [form] = Form.useForm();
+  const dispatch = useDispatch();
+  const searchParams = useSelector((state) => state.search.searchParams);
+  const [form] = Form.useForm();
 
-    useEffect(() => {
-        form.setFieldsValue(searchParams);
-    }, [searchParams]);
+  useEffect(() => {
+    form.setFieldsValue(searchParams);
+  }, [searchParams]);
 
-    const onValuesChange = (changedValues, allValues) => {
-        dispatch(setSearchParams(allValues));
-    };
+  const onValuesChange = (changedValues, allValues) => {
+    dispatch(setSearchParams(allValues));
+  };
 
-    const onFinish = () => {
-        const values = form.getFieldsValue();
-        dispatch(setIsSearching(true));
-        fetchData(
-            `${BASE_URL}/announcement/search`,
-            {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ filter: values, page: 1, size: 10 }),
-            },
-            true,
-            () => {}, // setLoading function
-            () => {}, // setSearchResults function
-            () => {}  // setTotal function
-        );
-    };
-
-    return (
-        <Form
-            form={form}
-            layout="inline"
-            onValuesChange={onValuesChange}
-            onFinish={onFinish}
-            style={{ display: 'flex', justifyContent: 'center' }}
-        >
-            <Form.Item name="title">
-                <Input placeholder="Search by title" />
-            </Form.Item>
-            <Form.Item>
-                <Button type="primary" htmlType="submit">
-                    Search
-                </Button>
-            </Form.Item>
-        </Form>
+  const onFinish = () => {
+    const values = form.getFieldsValue();
+    dispatch(setIsSearching(true));
+    fetchData(
+      `${BASE_URL}/announcement/search`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ filter: values, page: 1, size: 10 }),
+      },
+      true,
+      () => {}, // setLoading function
+      () => {}, // setSearchResults function
+      () => {} // setTotal function
     );
+  };
+  const handleFunction = (e) => {
+    console.log(e.target.value);
+  };
+
+  return (
+    <Form
+      form={form}
+      layout="inline"
+      onValuesChange={onValuesChange}
+      onFinish={onFinish}
+      style={{ display: "flex", justifyContent: "center" }}
+    >
+      <Form.Item name="title">
+        <Input
+          placeholder="Search by title"
+          onInput={(e) => handleFunction(e)}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Search
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default SearchFilter;
